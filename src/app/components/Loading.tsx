74e8cc825ba9a384dbf4cf65b34c04c3e485934e
Anthony Collette <100pour100inde@gmail.com>
2024-05-19 00:34:13 +0200

    add loading component

---
+++ src/app/components/Loading.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from './Container';
+
+const Loading = () => {
+    return (
+        <div>
+            <Container>
+                <h1>LOADING</h1>
+            </Container>
+        </div>
+    );
+};
+
+export default Loading;
