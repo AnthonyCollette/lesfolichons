1ccb465827e9156cf949d26001a0dc86c7ecbbe9
Anthony Collette <100pour100inde@gmail.com>
2024-05-19 22:27:30 +0200

    dynamic render

---
+++ src/app/api/remove-pet/route.ts
@@ -0,0 +1,18 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+
+export const dynamic = 'force-dynamic'
+
+export default async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get('id');
+
+  try {
+    await sql`DELETE FROM pets WHERE id=${id}`
+  } catch (error) {
+    return NextResponse.json({error}, {status: 500})
+  }
+
+  const pets = await sql`SELECT * FROM pets`
+  return NextResponse.json({pets}, {status: 200});
+}
