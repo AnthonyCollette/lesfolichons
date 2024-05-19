4984770f5232b1fde7b9cada8388cd534f95d2f6
Anthony Collette <100pour100inde@gmail.com>
2024-05-19 22:25:47 +0200

    dynamic render

--- src/app/api/get-pets/route.ts
+++ src/app/api/get-pets/route.ts
@@ -1,9 +1,11 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
+import { unstable_noStore as noStore } from 'next/cache';
 
 export const dynamic = 'force-dynamic'
 
 export async function GET(request: Request) {
+    noStore()
     const {rows} = await sql`SELECT * FROM Pets`
     return NextResponse.json(rows);
 }
