0ab98bac85d4e848a4d1617c271a4cf2c9625f05
Anthony Collette <100pour100inde@gmail.com>
2024-05-20 02:00:11 +0200

    update sliders

--- src/app/components/HomepageSlider.tsx
+++ src/app/components/HomepageSlider.tsx
@@ -7,6 +7,7 @@ import slide1 from '../assets/images/slide1.jpg';
 import slide2 from '../assets/images/slide2.jpg';
 import slide3 from '../assets/images/slide3.jpg';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export function HomepageSlider() {
 
@@ -23,21 +24,21 @@ export function HomepageSlider() {
                 <Image src={slide1} alt="Cochons d'Inde et un ananas" layout='fill' objectFit='cover' />
                 <Container>
                     <h3>Nos missions</h3>
-                    <button className='btn btn--primary'>Présentation de l'association <span></span></button>
+                    <Link href="/association" className='btn btn--primary'>Présentation de l'association <span></span></Link>
                 </Container>
             </div>
             <div>
                 <Image src={slide2} alt="Cochons d'Inde et un ananas" layout='fill' objectFit='cover' />
                 <Container>
                     <h3>Nos chons à l'adoption</h3>
-                    <button className='btn btn--primary'>Adoptez un de nos protégés <span></span></button>
+                    <Link href="/chons" className='btn btn--primary'>Adoptez un de nos protégés <span></span></Link>
                 </Container>
             </div>
             <div>
                 <Image src={slide3} alt="Cochons d'Inde et un ananas" layout='fill' objectFit='cover' />
                 <Container>
                     <h3>Faire un don</h3>
-                    <button className='btn btn--primary'>Nous aider <span></span></button>
+                    <Link href="/donate" className='btn btn--primary'>Nous aider <span></span></Link>
                 </Container>
             </div>
         </Carousel>
